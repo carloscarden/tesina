924fb075a54360eaf409d83c53e6af7e0a0666b4
unknown <cardencarlos90@gmail.com>
2024-01-03 18:52:40 -0300

    cambio nombre

--- tests/Primeras3ReglasTest.py
+++ tests/Primeras3ReglasTest.py
@@ -5,16 +5,33 @@ class Primeras3ReglasTest(unittest.TestCase):
 
     def regla1():
 
-        '''  Verbs give origin to facts '''
+        '''  Verbs give origin to facts 
+ 
+           entrada: List[Lel]
+           salida: List[Lel] tq Lel sea verbo
+           
+        
+        '''
         pass
 
     def regla2():
 
-        '''  Numerical objects and subjects of verbs give origin to measures. '''
+        '''  Numerical objects and subjects of verbs give origin to measures. 
+        
+           entrada: notion verb, List[Lel]
+           salida:  List[Lel] tq Lel sea el numerical object del verbo
+        
+        '''
+
         pass
 
     def regla3():
 
-        '''Categorical objects and subjects of verbs give origin to dimensions.'''
+        '''Categorical objects and subjects of verbs give origin to dimensions.
+        
+           entrada: notion verb, List[Lel]
+           salida:  List[Lel] tq Lel sea el categorical object del verbo
+
+        '''
         pass
 
--- tests/Siguientes4ReglasTest.py
+++ tests/Siguientes4ReglasTest.py
@@ -7,17 +7,35 @@ class Siguientes4ReglasTest(unittest.TestCase):
 
     def regla4():
 
-        '''Categorical objects and subjects of objects or subjects give origin to levels'''
+        '''Categorical objects and subjects of objects or subjects give origin to levels
+        
+           entrada: notion del subject, List[Lel]
+           salida:  List[Lel] tq Lel sea el categorical object del sujeto
+        '''
         pass
 
 
     def regla5():
 
-        '''Numerical objects and subjects of objects or subjects give origin to properties.'''
+        '''Numerical objects and subjects of objects or subjects give origin to properties.
+          
+           entrada: notion del subject, List[Lel]
+           salida:  List[Lel] tq Lel sea el numerical object del sujeto
+        
+        '''
         pass
 
 
     def regla6():
+
+        '''Numerical objects and subjects of objects or subjects give origin to properties.
+          
+           
+        '''
+        pass
+
+
+    def regla7():
 
         '''Plural objects and subjects give origin to multiple arcs'''
         pass
